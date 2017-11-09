1289353191a2fe1f6a3115bf58321bdb9804bde1
Baptiste <Baptiste@MacBook-Pro-de-user-2.local>
2017-11-09 14:38:59 +0100

    test change replacing a by div

--- __tests__/index.js
+++ __tests__/index.js
@@ -9,7 +9,7 @@ describe('Interactive rater: <Rater total={5} rating={2} />', () => {
   const rater = mount(<Rater total={5} rating={2} />)
   it('renders 5 stars(2 active)', () => {
     expect(rater.find('.react-rater').length).toEqual(1)
-    expect(rater.find('a').length).toEqual(5)
+    expect(rater.find('div').length).toEqual(5)
     expect(rater.find('.is-active').length).toEqual(2)
   })
   it('renders 3 active stars when rate 3', () => {
