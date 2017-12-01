7079855c444f61f2480afa80826146e982c86807
Etienne Lambert <etienne.lambert@wanadoo.fr>
2017-12-01 16:46:03 +0100

    Adding typescript definitions.

---
+++ src/index.d.ts
@@ -0,0 +1,24 @@
+// TypeScript Version: 2.3
+
+import { Component, SyntheticEvent } from 'react'
+
+interface StarProps {
+    isActive?: boolean
+    isActiveHalf?: boolean
+    willBeActive?: boolean
+    isDisabled?: boolean
+}
+
+export class Star extends Component<StarProps> {
+}
+
+interface RaterProps {
+    total?: number
+    rating?: number
+    interactive?: boolean
+    children?: Component[]
+    onRate?(rating:number, event:SyntheticEvent<number>): void
+}
+
+export default class Rater extends Component<RaterProps> {
+}
